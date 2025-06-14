625c4018a43fffbb0cd8d3432dc85cc19aabf4ab
flush36 <thiagoferreira.dev@gmail.com>
2025-06-14 08:35:44 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Github Actions with Java project
+
+Exemplo de aplicação Java com Maven, JUnit e pipeline CI/CD no GitHub Actions.
---
+++ src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    void testSubtract() {
+        assertEquals(1, calculator.subtract(3, 2));
+    }
+}
